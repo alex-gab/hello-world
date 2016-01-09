33a0e60a923015d2a9dc7a566c6109d57601aec0
dcstamate <cristi_stamate0383@yahoo.com>
2016-01-09 10:58:16 +0200

    SONAR test

--- src/main/java/ro/soft/helloworld/App.java
+++ src/main/java/ro/soft/helloworld/App.java
@@ -10,5 +10,6 @@ public class App
     {
         System.out.println( "Hello World!" );
         System.out.println( "Hello World! test sonar warn" );
+        System.out.println( "Hello World! test sonar warn 1" );
     }
 }
