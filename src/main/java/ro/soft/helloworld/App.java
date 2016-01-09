7faf32eec6e8a25f72635bb603d4ee11a657152f
Alex Dragoi <dragoi.alex@gmail.com>
2016-01-09 14:23:07 +0200

    My first commit for App.java (Alex)

--- src/main/java/ro/soft/helloworld/App.java
+++ src/main/java/ro/soft/helloworld/App.java
@@ -11,5 +11,6 @@ public class App
         System.out.println( "Hello World!" );
         System.out.println( "Hello World! test sonar warn" );
         System.out.println( "Hello World! test sonar warn 1" );
+	System.out.println( "Hello World! test sonar warn 2" );
     }
 }
